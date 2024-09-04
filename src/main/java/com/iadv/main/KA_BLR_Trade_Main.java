6854d888bccedf191cb99d95a1f891457e9f6a50
Abhishek Singh <abhishek@indiaadvocacy.com>
2024-09-04 20:54:03 +0530

    test

--- src/main/java/com/iadv/main/KA_BLR_Trade_Main.java
+++ src/main/java/com/iadv/main/KA_BLR_Trade_Main.java
@@ -19,6 +19,7 @@ public class KA_BLR_Trade_Main {
 			System.out.println(xlsxMapHeader);
 			
 			System.out.println(xlsxMapCol.get(2).get(5));
+			System.out.println(xlsxMapCol.get(2).get(6));
 			System.out.println(xlsxMapHeader.get(2).get("Company Category"));
 
 		
